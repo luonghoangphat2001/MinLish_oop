7635eb5e1ed12c2ef22720ce716aa1e2efcaf1dc
Phat Luong <phat.luong@hdwebsoft.dev>
2026-04-10 21:09:50 +0700

    fix update deploy.yml version php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * MinLish - Laravel Bridge for cPanel
+ * This file allows Laravel to run from the root directory without complex .htaccess rules.
+ */
+
+require __DIR__.'/public/index.php';
